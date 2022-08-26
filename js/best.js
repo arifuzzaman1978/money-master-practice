72efe4fbf21f6e9cb92ac1ee58c1a371bedd3656
arifuzzaman1978 <ariffashion@gmail.com>
2022-08-27 01:57:07 +0600

    sending to my assignment

---
+++ js/best.js
@@ -0,0 +1,5 @@
+document.getElementById('select-messi').addEventListener('click', function(){
+  const selectMessi = document.getElementById('select-messi');
+  const selectMessiString = selectField.value;
+  const selectField = parseFloat (selectMessiString);
+})
